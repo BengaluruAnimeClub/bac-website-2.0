db8d6ceecca097ed4fa2d2ea5a48f0c39ba938b1
BangaloreAnimeClub <archives.bengaluruanimeclub@gmail.com>
2025-02-09 21:10:56 +0530

    refactoring and fix for unique state

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -2,21 +2,22 @@
 
 import { Metadata } from "next";
 import { useState, useEffect } from "react";
-import { textNodes, TextNode, Option } from "./textNodes"; 
+import { Scenes, Scene, Option } from "./textNodes"; 
 
 export default function Game() {
 
-  const [currentText, setCurrentText] = useState<number>(1);
-  const currentNode: TextNode | undefined = textNodes.find(
-    (node) => node.id === currentText
+  const [currentScene, setCurrentScene] = useState<number>(1);
+
+  const currentNode: Scene | undefined = Scenes.find(
+    (node) => node.id === currentScene
   );
 
   if (!currentNode) {
-    return <div>Error: Node not found</div>; 
+    return <div>Error: Scene not found</div>; 
   }
 
-  const handleOptionSelect = (nextText: number) => {
-    setCurrentText(nextText);
+  const handleOptionSelect = (nextScene: number) => {
+    setCurrentScene(nextScene);
   };
 
   return (
@@ -24,20 +25,17 @@ export default function Game() {
       <h1 className="block font-black text-4xl lg:text-5xl text-center mb-10">
         BAC Love Story
       </h1>
-
-      <div className="columns-1 md:columns-2 lg:columns-2 w-fit gap-4 mx-auto space-y-4">
-        
-      </div>
       
       <div className="flex flex-col mx-auto p-5 border border-gray-300 rounded-lg">
         <div className="flex-1 overflow-y-auto mb-2">
+          <b>Scene #{currentNode.id}</b>
           <p>{currentNode.text}</p>
         </div>
         <div className="flex flex-col lg:flex-row gap-2 justify-center">
-          {currentNode.options.map((option: Option) => (
+          {currentNode.options.map((option: Option, index) => (
             <button
-              key={option.nextText}
-              onClick={() => handleOptionSelect(option.nextText)}
+              key={`${currentNode.id}-${index}`}
+              onClick={() => handleOptionSelect(option.nextScene)}
               className="px-4 py-2 bg-[#ea4167] text-white text-center text-base cursor-pointer rounded-md"
             >
               {option.text}
