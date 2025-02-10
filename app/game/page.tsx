217c78a0a7023f18e043019e7b272e4759efdad2
BangaloreAnimeClub <archives.bengaluruanimeclub@gmail.com>
2025-02-10 19:37:59 +0530

    use placeholder image for ending scenes

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -5,6 +5,14 @@ import { useState, useEffect } from "react";
 import Image from 'next/image';
 import { Scenes, Scene, Option } from "./textNodes"; 
 
+const imagePaths: { [key: number]: string } = {
+  0: "/images/game/placeholder.jpeg",
+  101: "/images/game/placeholder.jpeg",
+  102: "/images/game/placeholder.jpeg",
+  103: "/images/game/placeholder.jpeg",
+  104: "/images/game/placeholder.jpeg",
+};
+
 export default function Game() {
 
   const [currentScene, setCurrentScene] = useState<number>(0);
@@ -30,6 +38,10 @@ export default function Game() {
       <div className="flex flex-col mx-auto p-5 border border-gray-300 rounded-lg">
         <div className="flex-1 overflow-y-auto mb-2">
 
+          {currentNode.id === 0 && (
+            <center><b>Title Screen</b></center>
+          )}
+
           {currentNode.id > 0 && (
             <>
               <center><b>Scene #{currentNode.id}</b></center>
@@ -37,17 +49,14 @@ export default function Game() {
             </>
           )}
 
-          {currentNode.id === 0 && (
-            <>
-              <center><b>Title Screen</b></center>
+          {(currentNode.id === 0 || currentNode.id > 100) && (
               <Image 
-                src="/images/game/placeholder.jpeg" 
+                src={imagePaths[currentNode.id]} 
                 alt="Scene Image" 
                 width={500}
                 height={300}
                 className="mt-4 mb-4 mx-auto w-full lg:w-1/2" 
               />
-            </>
           )}
 
         </div>
