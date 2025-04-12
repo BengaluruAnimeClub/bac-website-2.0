c3dfe343d0f5b971cb0975da0542565601609461
BangaloreAnimeClub <archives.bengaluruanimeclub@gmail.com>
2025-04-13 01:52:05 +0530

    justified text in blog posts

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -84,8 +84,10 @@ export default async function PostPage({ params }: PostPageProps) {
         )}
       </div>
       <hr className="my-4 mt-2 mb-4" />
-      <MDXContent code={post.body} />
-      <p className="text-md mt-2 mb-0 text-muted-foreground">
+      <div className="text-justify">
+        <MDXContent code={post.body} />
+      </div>
+      <p className="text-md mt-2 mb-0 text-muted-foreground text-justify">
         <i>All content on this website is protected by copyright and may not be copied, distributed, or reproduced in any form without the express written consent from <span className="font-semibold">team@bac.moe</span>.</i>
       </p>
     </article>
