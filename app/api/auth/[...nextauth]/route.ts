15c9e07f31a06f1045d4183b59b6b808f54c92a0
Bangalore Anime Club <archives.bengaluruanimeclub@gmail.com>
2025-05-17 16:57:52 +0530

    undo twitter auth (#32)

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import TwitterProvider from "next-auth/providers/twitter";
 import { NextRequest, NextResponse } from "next/server";
 
 const handler = NextAuth({
@@ -13,11 +12,6 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID!,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET!,
-      version: "2.0", // Use Twitter OAuth 2.0
     }),
     // Add more providers here
   ],
