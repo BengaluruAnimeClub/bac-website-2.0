ae79b1149d2da3884c5e6a96776fca682afe7806
Aravind G. <aravindg1@iisc.ac.in>
2025-05-18 09:56:48 +0530

    Search improvements (#38)
    * unordered search
    
    * header parsing improvements
    
    * "showing results for"
    
    ---------
    
    Co-authored-by: BangaloreAnimeClub <archives.bengaluruanimeclub@gmail.com>

--- components/blog-search-index.ts
+++ components/blog-search-index.ts
@@ -4,227 +4,234 @@
 export const blogSearchIndex = [
   {
     "title": "Predict the release date of your favourite anime!",
+    "description": "Learn about animation producers and production lines in anime studios",
     "slug": "anime-industry-insight-feb25",
     "headers": []
   },
   {
     "title": "Anime Spotlight Apr'25",
+    "description": "×××HOLiC, Pluto",
     "slug": "anime-spotlight-apr25",
-    "headers": [
-      "Pluto",
-      "×××HOLiC"
-    ]
+    "headers": []
   },
   {
     "title": "Anime Spotlight Dec'24",
+    "description": "Given",
     "slug": "anime-spotlight-dec24",
-    "headers": [
-      "Given"
-    ]
+    "headers": []
   },
   {
     "title": "Anime Spotlight Feb'25",
+    "description": "Michiko & Hatchin",
     "slug": "anime-spotlight-feb25",
-    "headers": [
-      "Michiko & Hatchin"
-    ]
+    "headers": []
   },
   {
     "title": "Anime Spotlight Jan'25",
+    "description": "Kino's Journey, Kakushigoto",
     "slug": "anime-spotlight-jan25",
-    "headers": [
-      "Kakushigoto",
-      "Kino's Journey"
-    ]
+    "headers": []
   },
   {
     "title": "Anime Spotlight May'25",
+    "description": "Servant × Service",
     "slug": "anime-spotlight-may25",
-    "headers": [
-      "Servant × Service"
-    ]
+    "headers": []
   },
   {
     "title": "Anime Spotlight Nov'24",
+    "description": "Shirobako",
     "slug": "anime-spotlight-nov24",
-    "headers": [
-      "Shirobako"
-    ]
+    "headers": []
   },
   {
     "title": "Anime Which People Don’t Talk About These Days #2",
+    "description": "Romeo × Juliet, True Tears, Paranoia Agent, Air, Magic Knight Rayearth",
     "slug": "anime-which-people-dont-talk-about-2",
-    "headers": []
+    "headers": [
+      "2'\ndescription: \"Romeo × Juliet, True Tears, Paranoia Agent, Air, Magic Knight Rayearth\"\ndate: 2025-05-15",
+      "f05a78"
+    ]
   },
   {
     "title": "Anime Which People Don’t Talk About These Days #1",
+    "description": "Kyo Kara Maou!, The Story of Saiunkoku, Black Cat, Lovely Complex, Desert Punk",
     "slug": "anime-which-people-dont-talk-about",
-    "headers": []
+    "headers": [
+      "1'\ndescription: \"Kyo Kara Maou!, The Story of Saiunkoku, Black Cat, Lovely Complex, Desert Punk\"\ndate: 2025-04-17",
+      "f05a78"
+    ]
   },
   {
     "title": "BAC Love Story",
+    "description": "Game credits, and the making of BAC Love Story",
     "slug": "bac-love-story",
     "headers": [
-      "The Heart of Valentine's Day",
-      "The Heart of Valentine's Day",
-      "Story Concept - A Simple Idea, Big Dream",
-      "Story Concept - A Simple Idea, Big Dream",
-      "Characters - The Heartbeat of the Story"
+      "##",
+      "#",
+      "f05a78"
     ]
   },
   {
     "title": "BACademy Awards 2025",
+    "description": "The first BACademy Awards edition for anime and manga",
     "slug": "bacademy-awards-2025",
     "headers": [
       "Index",
-      "BACademy Awards 2025",
-      "Nomination and Award Process",
-      "Nomination Rules and Considerations",
-      "Category I: Character Awards"
+      "nomination-and-award-process)",
+      "nomination-rules-and-considerations)",
+      "category-i-character-awards)",
+      "best-main-character)"
     ]
   },
   {
     "title": "Kimi no Iro — A journey through Yamada's melodic kaleidoscope",
+    "description": "A retrospective of The Colors Within (Kimi no Iro) movie, and artwork by BAC artists",
     "slug": "colors-within",
     "headers": []
   },
   {
     "title": "Gimai Seikatsu - Complementarity at its finest",
+    "description": "",
     "slug": "gimaisei",
     "headers": []
   },
   {
     "title": "Naoko Yamada Spotlight: Heikemonogatari",
+    "description": "Special edition of the Anime Spotlight series in the backdrop of upcoming release of \"The Colors Within\" movie!",
     "slug": "heike-story",
     "headers": [
-      "Today's Question"
+      "191919"
     ]
   },
   {
     "title": "Naoko Yamada Spotlight: K-On!",
+    "description": "Special edition of the Anime Spotlight series in the backdrop of upcoming release of \"The Colors Within\" movie!",
     "slug": "k-on",
     "headers": [
-      "Today's Question"
+      "191919"
     ]
   },
   {
     "title": "Leaked Chats: Gurukiran and Kanapon’s Secret Duet!",
+    "description": "Gurukiran Shetty and Kanapon chat about their upcoming performances at Japan Habba 🎤✨",
     "slug": "leaked-chats-japan-habba",
     "headers": []
   },
   {
     "title": "Naoko Yamada Spotlight: Liz and the Blue Bird",
+    "description": "Special edition of the Anime Spotlight series in the backdrop of upcoming release of \"The Colors Within\" movie!",
     "slug": "liz-and-blue-bird",
     "headers": [
-      "First disjoint, then joint",
-      "Today's Question"
+      "191919"
     ]
   },
   {
     "title": "Manga Spotlight Apr'25",
+    "description": "The Urban Legend Files, My Dearest Self With Malice Aforethought, Blue Flag",
     "slug": "manga-spotlight-apr25",
-    "headers": [
-      "Blue Flag",
-      "My Dearest Self With Malice Aforethought",
-      "The Urban Legend Files"
-    ]
+    "headers": []
   },
   {
     "title": "Manga Spotlight Aug'24",
+    "description": "Blank Canvas: My So-Called Artist’s Journey, Princess Jellyfish, Love at Fourteen",
     "slug": "manga-spotlight-aug24",
-    "headers": [
-      "Blank Canvas: My So-Called Artist’s Journey",
-      "Princess Jellyfish",
-      "Love at Fourteen"
-    ]
+    "headers": []
   },
   {
     "title": "Manga Spotlight Dec'24",
+    "description": "Message to Adolf, Stargazing Dog, Star Protector Dog",
     "slug": "manga-spotlight-dec24",
-    "headers": [
-      "Stargazing Dog & Star Protector Dog",
-      "Message to Adolf"
-    ]
+    "headers": []
   },
   {
     "title": "Manga Spotlight Feb'25",
+    "description": "GTO: The Early Years, Suicide Parabellum",
     "slug": "manga-spotlight-feb25",
-    "headers": [
-      "Suicide Parabellum",
-      "GTO: The Early Years"
-    ]
+    "headers": []
   },
   {
     "title": "Manga Spotlight Jan'25",
+    "description": "Toilet-bound Hanako-kun, The Summit of the Gods",
     "slug": "manga-spotlight-jan25",
-    "headers": [
-      "The Summit of the Gods",
-      "Toilet-bound Hanako-kun"
-    ]
+    "headers": []
   },
   {
     "title": "Manga Spotlight Jul'24",
+    "description": "Bless, Mizu wa Umi ni Mukatte Nagareru",
     "slug": "manga-spotlight-jul24",
-    "headers": [
-      "Bless",
-      "Mizu wa Umi ni Mukatte Nagareru"
-    ]
+    "headers": []
   },
   {
     "title": "Manga Spotlight May'25",
+    "description": "Mr. Mallow Blue",
     "slug": "manga-spotlight-may25",
-    "headers": [
-      "Mr. Mallow Blue"
-    ]
+    "headers": []
   },
   {
     "title": "Manga Spotlight Nov'24",
+    "description": "Shiawase Toriming, The Summer Hikaru Died",
     "slug": "manga-spotlight-nov24",
-    "headers": [
-      "The Summer Hikaru Died",
-      "Shiawase Toriming"
-    ]
+    "headers": []
   },
   {
     "title": "Manga Spotlight Oct'24",
+    "description": "The Voynich Hotel, Kanata no Astra, Spirit Circle",
     "slug": "manga-spotlight-oct24",
-    "headers": [
-      "The Voynich Hotel",
-      "Kanata no Astra",
-      "Spirit Circle"
-    ]
+    "headers": []
   },
   {
     "title": "ME1: Honobu Yonezawa's Gifu Day 1",
+    "description": "A travelogue/diary entry masquerading as an exploration of Shoushimin and Hyouka, anime adaptations of Honobu Yonezawa’s mystery novels.",
     "slug": "me1-honobu-yonezawa-gifu-day1",
     "headers": [
       "Day 1",
-      "Tangent 1: More pictures of Chuusetsu Bridge",
-      "Tangent 2: Some Gifu Carposting",
-      "Tangent 3: An advert by the Polish Tourism Org on a train",
-      "Tangent 4: A crepe vending machine"
+      "# Tangent 1: More pictures of Chuusetsu Bridge",
+      "# Tangent 2: Some Gifu Carposting",
+      "# Tangent 3: An advert by the Polish Tourism Org on a train",
+      "# Tangent 4: A crepe vending machine"
     ]
   },
   {
     "title": "A Message from Japan Habba",
+    "description": "Discover how a shared passion for Japanese culture has created a thriving community in Bengaluru, culminating in the expansive Japan Habba festival",
     "slug": "message-from-japan-habba",
     "headers": []
   },
   {
     "title": "BAC Online Secret Santa: Part-I",
+    "description": "Part-I of personalised anime recommendations by and for BAC members, from the Online Secret Santa event!",
     "slug": "secret-santa-2024-part-1",
-    "headers": []
+    "headers": [
+      "191919"
+    ]
   },
   {
     "title": "BAC Online Secret Santa: Part-II",
+    "description": "Part-II of personalised anime recommendations by and for BAC members, from the Online Secret Santa event!",
     "slug": "secret-santa-2024-part-2",
-    "headers": []
+    "headers": [
+      "191919"
+    ]
   },
   {
     "title": "Naoko Yamada Spotlight: A Silent Voice",
+    "description": "Special edition of the Anime Spotlight series in the backdrop of upcoming release of \"The Colors Within\" movie!",
     "slug": "silent-voice",
     "headers": [
-      "Today's Question"
+      "191919"
+    ]
+  },
+  {
+    "title": "Spring 2025 Season Recommendations",
+    "description": "The most exciting shows of the spring 2025 anime season recommended by BAC members",
+    "slug": "spring-2025",
+    "headers": [
+      "Anne Shirley",
+      "Kowloon Generic Romance",
+      "Yaiba",
+      "Rock is a Lady's Modesty",
+      "Food for the Soul"
     ]
   },
   {
@@ -234,14 +241,19 @@ export const blogSearchIndex = [
   },
   {
     "title": "Naoko Yamada Spotlight: Tamako Love Story",
+    "description": "Special edition of the Anime Spotlight series in the backdrop of upcoming release of \"The Colors Within\" movie!",
     "slug": "tamako-love-story",
     "headers": [
-      "Today's Question"
+      "191919"
     ]
   },
   {
     "title": "Terumin「テルミン」",
+    "description": "Introducing BAC's techie mascot: Terumin!",
     "slug": "terumin",
-    "headers": []
+    "headers": [
+      "! ) sequence that starts a script file.",
+      "Character Profile"
+    ]
   }
 ];
