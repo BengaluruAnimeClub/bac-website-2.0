77ddf6036ee92c54cc0f6a1198cfc266e1bd2256
BangaloreAnimeClub <archives.bengaluruanimeclub@gmail.com>
2025-05-28 13:07:33 +0530

    add back love story in nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -112,6 +112,15 @@ export function MainNav() {
         </div>
       </div>
       <Link
+        href="/game"
+        className={cn(
+          "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block",
+          pathname === "/game" ? "text-foreground" : "text-foreground/60"
+        )}
+      >
+        Love Story
+      </Link>
+      <Link
         href="/socials"
         className={cn(
           "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block",
