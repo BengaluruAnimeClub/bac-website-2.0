f839f048457ce6693fc339b577a928ea249d47c7
Bengaluru Anime Club <archives.bengaluruanimeclub@gmail.com>
2025-05-30 12:39:42 +0530

    Cleanup: URL redirects + Delete Unused Images (#66)

---
+++ lib/redirects.ts
@@ -0,0 +1,6 @@
+// A lookup table for slug-to-URL redirections
+export const redirectTable: Record<string, string> = {
+  animespotlight: 'https://forms.gle/38ACSWeu41pBFZqW8',
+  mangaspotlight: 'https://forms.gle/mH9uv5naDSRXGZU29',
+  shop: 'https://rzp.io/rzp/BengaluruAnimeClub',
+};
