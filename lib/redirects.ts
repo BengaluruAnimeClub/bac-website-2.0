77fc582e09b8b19ac28f85af669978047c739413
BangaloreAnimeClub <archives.bengaluruanimeclub@gmail.com>
2025-05-30 12:11:44 +0530

    example

--- lib/redirects.ts
+++ lib/redirects.ts
@@ -1,5 +1,6 @@
 // A lookup table for slug-to-URL redirections
 export const redirectTable: Record<string, string> = {
+  example: 'https://example.com',
   MangaSpotlight: 'https://forms.gle/mH9uv5naDSRXGZU29',
   AnimeSpotlight: 'https://forms.gle/38ACSWeu41pBFZqW8',
   shop: 'https://rzp.io/rzp/BengaluruAnimeClub',
