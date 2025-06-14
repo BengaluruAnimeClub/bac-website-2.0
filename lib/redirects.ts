8eef5d2f6993f9fdace4ed2a49b194b9cf65e188
Bengaluru Anime Club <archives.bengaluruanimeclub@gmail.com>
2025-06-14 17:07:38 +0530

    Update BFC link (#72)

--- lib/redirects.ts
+++ lib/redirects.ts
@@ -3,6 +3,7 @@ export const redirectTable: Record<string, string> = {
   animespotlight: 'https://forms.gle/38ACSWeu41pBFZqW8',
   mangaspotlight: 'https://forms.gle/mH9uv5naDSRXGZU29',
   shop: 'https://rzp.io/rzp/BengaluruAnimeClub',
+  food: 'https://chat.whatsapp.com/IctnGQfw4OkFlawMl0qmBS',
   collaborationguidelines: '/contact-us/collaboration-guidelines',
   'collaboration-guidelines': '/contact-us/collaboration-guidelines',
 };
